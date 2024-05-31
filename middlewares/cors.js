a10c3b6be75bc5aa72e51b950b90b3688721b652
LidelsPastrin <149607624+LidelsPastrin@users.noreply.github.com>
2024-05-31 11:32:36 +0300

    start

---
+++ middlewares/cors.js
@@ -0,0 +1,23 @@
+const allowedCors = [
+    'https://practicum.yandex.ru',
+    'https://students-projects.ru',
+    'http://localhost:3000',
+    'http://localhost:3001',
+    'http://158.160.166.160:3001',
+    'http://158.160.166.160:3000/',
+    'https://back-warrior.nomorepartiesco.ru',
+    'https://front-warrior.nomorepartiesco.ru'
+  ];
+
+ function cors(req, res, next) {
+  const { origin } = req.headers;
+
+  if (allowedCors.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE");
+  res.header("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers, Authorization");
+  next();
+}
+
+module.exports =  cors;
